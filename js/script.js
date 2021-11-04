c7da6b3a06844f5439fd18e8938f3e9e548f9a8e
Dmitriy0101 <Dmitriy0101>
2021-11-04 22:51:45 +0200

    blog

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ $('.slider').slick({
             }
         },
         {
-            breakpoint: 1100,
+            breakpoint: 1150,
             settings: {
                 dots: false,
                 slidesToShow: 3,
@@ -47,16 +47,28 @@ $('.slider').slick({
             }
         },
         {
-            breakpoint: 900,
+            breakpoint: 1000,
             settings: {
-                dots: false,
+                arrows:false,
+                dots: true,
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 870,
+            settings: {
+                arrows:false,
+                dots: true,
                 slidesToShow: 2,
                 slidesToScroll: 1
             }
         },
         {
-            breakpoint: 570,
+            breakpoint: 600,
             settings: {
+                arrows: false,
+                dots: false,
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
@@ -65,6 +77,8 @@ $('.slider').slick({
 });
 // <------ Slider END ------>
 
+
+// <------ MOBILE MENU START ------>
 const menuBtnRef = document.querySelector("[data-menu-button]");
 const menu = document.querySelector("[data-menu]");
 const mobileMenuClose = document.querySelector("[data-menu-close]");
@@ -77,13 +91,13 @@ function onCloseMenu() {
     document.body.classList.toggle('modal-open');
 }
 
-function onEscKeyPress(e){
+function onEscKeyPress(e) {
     if (e.code === 'Escape') {
         onCloseMenu();
     }
 }
 
-if(menuBtnRef && menu && mobileMenuClose) {
+if (menuBtnRef && menu && mobileMenuClose) {
     menuBtnRef.addEventListener("click", () => {
         onCloseMenu();
         window.addEventListener('keydown', onEscKeyPress);
@@ -101,3 +115,15 @@ if(menuBtnRef && menu && mobileMenuClose) {
         }
     });
 }
+// <------ MOBILE MENU END ------>
+
+
+// <------ SEARCH START ------>
+$(document).ready(function () {
+    var searchBlock = $('#form');
+    $(document).on('click', '#open', function () {
+        searchBlock.slideToggle();
+        return false;
+    });
+});
+// <------ SEARCH END ------>
